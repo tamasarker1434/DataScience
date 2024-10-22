2a31f7e3e9b6cdf751729abb9172fd7c564c1d3b
Tama Sarker <tamasarker112@gmail.com>
2024-10-22 09:09:25 +0200

    Update

--- SelfStudy/tutorial.py
+++ SelfStudy/tutorial.py
@@ -1,4 +1,25 @@
 import numpy as np
-a = np.array([1,2,3,4,5])
-print(a)
-print(type(a))
+#1D array
+# a = np.array([1,2,3,4,5])
+# print(a)
+# print(type(a))
+
+#m-D array
+# a_mult = np.array([
+#     [
+#         [1,2,3,4],
+#         [5,6,7,8],
+#         [9,10,11,12]
+#     ],
+#     [
+#         [11,12,13,14],
+#         [15,16,17,18],
+#         [19,20,21,22]
+#     ]
+# ])
+#
+# print(a_mult.shape)
+# print(a_mult.ndim)
+# print(a_mult.size)
+# print(a_mult.dtype)
+#Different type
